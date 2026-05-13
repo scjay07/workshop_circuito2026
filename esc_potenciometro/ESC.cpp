a21e72a097a04b2ba3fc2c59de0e1d50e5a43063
Rodrigo Haruna <Rodrigo.haruna@gmail.com>
2026-05-13 11:16:01 -0300

    Update ESC.cpp

--- esc_potenciometro/ESC.cpp
+++ esc_potenciometro/ESC.cpp
@@ -1,15 +1,21 @@
 #include "ESC.h"
 
-void inicializar() {
-    esc.attach(3);
+ESC::ESC(int pin){
+    this->pin = pin;
+}
+
+void ESC::inicializar() {
+    esc.attach(pin);
     esc.write(180);
     delay(100);
     esc.write(0);
     delay(100);
 }
 
-void mandar_velocidade(int speed, int maxPot) {
-    if(speed > maxPot) speed = maxPot;
+void ESC::mandar_velocidade(int speed, int maxPot) {
     speed = map(speed,0,1023,0,maxPot);
+    if(speed > maxPot) speed = maxPot;
+    if(speed < 0) speed = 0;
+    
     esc.write(speed);
 }
